f5c556766323a1db910f45711ac4ca98a489a4c4
Richard Newman <richardnewman@gogolabs.net>
2015-12-27 11:03:22 -0700

    Rework the timecode filter logic to use the Buzz method instead of your custom code.

--- app/scripts/filters/timecode.js
+++ app/scripts/filters/timecode.js
@@ -1,22 +1,7 @@
 (function() {
   function timecode() {
     return function(argSeconds) {
-      var totalSeconds = Math.floor(Number.parseFloat(argSeconds));
-
-      if (Number.isNaN(totalSeconds)) {
-        return '-:--';
-      }
-
-      var minutes = Math.floor(totalSeconds / 60.0);
-      var seconds = totalSeconds % 60;
-
-      var output = minutes + ':';
-      if (seconds < 10) {
-        output += '0';
-      }
-      output += seconds;
-
-      return output;
+      return buzz.toTimer(argSeconds, false);
     };
   }
 
